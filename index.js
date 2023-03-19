6a1b1d211b671e3a370d2618f6b68d895fb62cd8
Kelvin Muturi <muturikelvin893@gmail.com>
2023-03-19 10:38:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers, name) {
+ const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+ return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, letters) {
+    const matchingDrivers = drivers.filter(driver => driver.startsWith(letters));
+    return matchingDrivers;
+}
+
+const driversAndTown = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(driversAndTown, name) {
+    const matchingDrivers = driversAndTown.filter(driver => driver.name === name);
+    return matchingDrivers;
+  }
